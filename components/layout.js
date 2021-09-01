956ac191cc4a5b2e526c9b2d4fc019b170bf814b
Елизавета Смолина <lizasmolina0211@gmail.com>
2021-09-01 03:52:56 +0200

    Layout creation

--- components/layout.js
+++ components/layout.js
@@ -1,11 +1,12 @@
 import styles from '../styles/Home.module.css'
 import { Image } from "@chakra-ui/react"
+import { Link } from "@chakra-ui/react"
 
 const Layout = ({ children }) => {
     return (
         <div>
             <header className={styles.header}>
-                <Image boxSize="30px" src="https://image.flaticon.com/icons/png/512/814/814513.png" display = 'inline-block'/>      Countries of the World
+            <Link href='/'><Image boxSize="30px" src="https://image.flaticon.com/icons/png/512/814/814513.png" display = 'inline-block'/></Link>        Countries of the World
             </header>
             {children}
         </div>
